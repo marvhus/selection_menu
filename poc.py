a69d12e0123c7d3253fb3fe049ddb3f1e1c4846b
Martin <marvahus@proton.me>
2022-10-23 01:01:16 +0200

    Added more functionality
    + Added keybind names
    + Added action names
    + Added quit/exit keybind
    + Added quit/exit action
    + Added quit/exit callback
    + Added functionality to print the menu and keybinds
    + Fixed bug of it not using the first key you press
    + Fixed selection bug where it went the wrong way

--- poc.py
+++ poc.py
@@ -1,3 +1,5 @@
+import os
+
 # https://stackoverflow.com/a/40492207
 class _Getch:
     """
@@ -40,53 +42,73 @@ class Menu:
         self.selection = 0
         self.getch = _Getch()
         self.keybinds = {
-            'w': self.select_up,
-            's': self.select_down,
-            ' ': self.make_selection,
+            'w': {'callback': self.select_up,      'name': 'select up'},
+            's': {'callback': self.select_down,    'name': 'select down'},
+            ' ': {'callback': self.make_selection, 'name': 'make selection'},
+            'q': {'callback': self.exit_menu,      'name': 'exit'},
         }
         self.actions = {
-            0: self.option_1,
-            1: self.option_2,
+            0: {'callback': self.option_1, 'name': 'option 1'},
+            1: {'callback': self.option_2, 'name': 'option 2'},
+            2: {'callback': self.exit_menu, 'name': 'Exit'},
         }
 
     # Helper
     def change_selection(self, change):
         self.selection += change
         self.selection %= len(self.actions.keys())
-        print(self.selection)
+        self.draw()
         
     # Callbacks
 
     ## Selection changing
     def select_up(self):
-        self.change_selection(1)
+        self.change_selection(-1)
         
     def select_down(self):
-        self.change_selection(-1)
+        self.change_selection(+1)
         
     def make_selection(self):
         if self.selection in self.actions.keys():
-            self.actions[self.selection]()
+            self.actions[self.selection]['callback']()
 
     ## Selection actions
     def option_1(self):
         print('ACTION: option_1')
 
     def option_2(self):
-        print('ACTION* option_2')
+        print('ACTION: option_2')
+
+    def exit_menu(self):
+        exit(0)
             
     # Call callback
     def use_keybind(self):
         if self.char in self.keybinds:
-            self.keybinds[self.char]()
-        else:
-            print(f'Unknown keybind "{self.char}"')
-        
+            self.keybinds[self.char]['callback']()
+        #else:
+            #print(f'Unknown keybind "{self.char}"')
+
+    # Draw menu
+    def draw(self):
+        # clear
+        os.system('cls' if os.name == 'nt' else 'clear')
+        #print('\033[H')
+        print('####### KEYBINDS #######')
+        for k, v in self.keybinds.items():
+            print(f'\t\t"{k}" --- {v["name"]}')
+        print('######### MENU #########')
+        for k, v in self.actions.items():
+            selected = "[x]" if self.selection == k else "[ ]"
+            print(f'\t\t{selected} --- {v["name"]}')
+        print('########################')
+
+    # Start menu
     def start(self):
-        self.char = self.getch()
+        self.draw()
+        self.char = ''
         while self.char != 'q':
             self.char = self.getch()
             self.use_keybind()
-                
         
 Menu().start()
